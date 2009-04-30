f03423437d3ee793c118b8b5b5ce08df712f74d1
retrogradeorbit <retrogradeorbit@1c1e6d6e-3548-11de-9f24-65e390db57b8>
2009-04-30 05:42:55 +0000

    Initial checkin

---
+++ func.pyx
@@ -0,0 +1,35 @@
+"""
+
+POISE
+=====
+Python Oldskool Ingame Sound Effects
+
+Support library. Quickly calculates buffers. manipulates buffers. 
+"""
+
+cdef int NUM_SINE_VALUES = 4096
+cdef np.ndarray SINE_TABLE = np.zeros([NUM_SINE_VALUES, 1], dtype=DTYPE)
+
+def fill_sine():
+    cdef int x
+    cdef float theta
+    for x in range(NUM_SINE_VALUES):
+        theta = float(x)*2.*math.pi/(float(NUM_SINE_VALUES))
+        SINE_TABLE[x] = math.sin(theta)
+
+fill_sine()
+
+def sin(float theta):
+    cdef float x = theta*float(NUM_SINE_VALUES)/(2.*math.pi) + 2.0 * math.pi / float(NUM_SINE_VALUES)
+    cdef float answer = SINE_TABLE[int(x)%NUM_SINE_VALUES]
+    return answer
+    
+def cos(float theta):
+    return sin(theta+math.pi/4.0)
+
+def dB(float gain=0.0):
+    cdef float db = 10.0 ** ( gain / 20.0 )
+    return db
+
+
+
