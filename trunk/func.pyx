87a604eb3aedd232418caf410d5b6e70e216ebe5
retrogradeorbit <retrogradeorbit@1c1e6d6e-3548-11de-9f24-65e390db57b8>
2009-04-30 06:56:52 +0000

    Fade envelope function

--- trunk/func.pyx
+++ trunk/func.pyx
@@ -17,8 +17,10 @@ def fill_sine():
         theta = float(x)*2.*math.pi/(float(NUM_SINE_VALUES))
         SINE_TABLE[x] = math.sin(theta)
 
+# run on import to fill table
 fill_sine()
 
+# calculate sin by looking up the closest value
 def sin(float theta):
     cdef float x = theta*float(NUM_SINE_VALUES)/(2.*math.pi) + 2.0 * math.pi / float(NUM_SINE_VALUES)
     cdef float answer = SINE_TABLE[int(x)%NUM_SINE_VALUES]
@@ -27,6 +29,7 @@ def sin(float theta):
 def cos(float theta):
     return sin(theta+math.pi/4.0)
 
+# turn decibel into a multiplier
 def dB(float gain=0.0):
     cdef float db = 10.0 ** ( gain / 20.0 )
     return db
