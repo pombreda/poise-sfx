6dce680bbfc4bd308c4111e3d6162eeeba1ec340
retrogradeorbit <retrogradeorbit@1c1e6d6e-3548-11de-9f24-65e390db57b8>
2009-05-02 04:28:10 +0000

    optimised cython code. refactoring.

--- func.pyx
+++ func.pyx
@@ -10,7 +10,8 @@ Support library. Quickly calculates buffers. manipulates buffers.
 cdef int NUM_SINE_VALUES = 8192
 cdef np.ndarray SINE_TABLE = np.zeros([NUM_SINE_VALUES, 1], dtype=DTYPE)
 
-def fill_sine():
+cdef fill_sine():
+    """this fills our static lookup array for our highspeed sin function"""
     cdef int x
     cdef float theta
     for x in range(NUM_SINE_VALUES):
@@ -21,22 +22,36 @@ def fill_sine():
 fill_sine()
 
 # calculate sin by looking up the closest value
-def sin(float theta):
+cdef sin(float theta):
     cdef float x = theta*float(NUM_SINE_VALUES)/(2.*math.pi) + 2.0 * math.pi / float(NUM_SINE_VALUES)
     cdef float answer = SINE_TABLE[int(x)%NUM_SINE_VALUES]
     return answer
     
-def sin_true(float theta):
+cdef sin_true(float theta):
     cdef float answer = math.sin(theta)
     return answer
     
-def cos(float theta):
+cdef cos(float theta):
     return sin(theta+math.pi/4.0)
 
 # turn decibel into a multiplier
-def dB(float gain=0.0):
+cdef db(float gain=0.0):
     cdef float db = 10.0 ** ( gain / 20.0 )
     return db
 
+def dB(float gain=0.0):
+    return db(gain)
 
-
+# window theta
+cdef frange( float var, float min, float max ):
+    cdef float wavelength = max - min
+    cdef int div
+    cdef int divexp
+    
+    # quickly bring back into range min to max
+    for divexp in range(5,0,-1):
+        div = 10 ** divexp
+        while var>wavelength*float(div):
+            var -= wavelength*float(div)
+            
+    return var
