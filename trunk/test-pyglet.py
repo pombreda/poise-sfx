7934ed160048627631a42a99084cee47e6453c58
retrogradeorbit <retrogradeorbit@1c1e6d6e-3548-11de-9f24-65e390db57b8>
2009-05-03 01:45:28 +0000

    save wav file done

--- trunk/test-pyglet.py
+++ trunk/test-pyglet.py
@@ -8,17 +8,26 @@ player.play()
 
 sfx = osc.sine(440.0, gain=0 )
 sfx.next()
-#envsfx = envelope.adsr(sfx,attack=2.0,decay=2.0,release=2.0,sustain=0.0)
-#envsfx.next()
+envsfx = envelope.adsr(sfx,attack=0.2,decay=0.2,sustain=0.4,release=2.0)
+envsfx.next()
 #poise.oscillators.append( (envsfx, -6) )
 
+#sfx = envsfx
+
 # render 1 second of 440 Hz sine
-buffer = sfx.send( (0,48000) )
-o=[]
-for i in buffer[:48000/440]:
-    o.append(float(int(i*500)+500)/10)
+#buffer = sfx.send( (0,48000) )
+##o=[]
+#for i in buffer[:48000/440]:
+#    o.append(float(int(i*500)+500)/10)
     
-print ",".join([str(a) for a in o])
+#print ",".join([str(a) for a in o])
+
+from poise import WaveFile
+
+wf = WaveFile()
+wf.oscillators.append( (envsfx, -1) )
+wf.save("test.wav",200000)
+
     
 import sys
 sys.exit()
